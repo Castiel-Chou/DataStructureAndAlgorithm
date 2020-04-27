626b4184af2814e334a96f54491f61b431e87465
Castiel-Chou <1024232950@qq.com>
2020-04-27 16:15:04 +0800

    feat: 完成Leecode33

---
+++ exercise/src/main/java/Leetcode33_Search.java
@@ -0,0 +1,63 @@
+/**
+ * @ClassName: Leetcode33_Search
+ * @Description:
+ *
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ *
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ *
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ *
+ * 你可以假设数组中不存在重复的元素。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ * 示例 2:
+ *
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author: Jokey Zhou
+ * @Date: 2020/4/27 16:10
+ * @赛博世界并不是辽阔的荒野，数据也不全是冰冷的记录，它是亲人的笑靥，它是我们的记忆。
+ */
+public class Leetcode33_Search {
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // 当在数组中找到了target时，返回数组下标
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < nums[right]) {
+                // 当中值小于最右值，表示中值右侧是有序的
+                // 判断mid往左走还是往右走
+                // 此处举例数组 [8,7,1,2,3,4,5]
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else { // 此处举例数组[4,5,6,7,8,0,1,2]
+                if (nums[mid] > target && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
